11080d38b805cc4c57981f8da35122d1df96c77b
Patrick <pkbruso@gmail.com>
2021-05-24 13:05:17 -0700

    Finished get_grid_average function to obtain average RGB value of grid area

--- shrink_ray.py
+++ shrink_ray.py
@@ -10,7 +10,8 @@ from simpleimage import SimpleImage
 def main():
     image = SimpleImage('images/landscape.jpg')
     image_copy = SimpleImage.blank(image.width, image.height)
-
+    get_grid_average(0, 0, image)
+    """
     y = 0
     x = 0
     new_x = 0
@@ -21,16 +22,7 @@ def main():
     num = 0
     while y < image.height:
         while x < image.width:
-            for i in range(x, x + 2):
-                for j in range(y, y + 2):
-                    pixel = image.get_pixel(i, j)
-                    red += pixel.red
-                    green += pixel.green
-                    blue += pixel.blue
-                    num += 1
-            pixel.red = red
-            pixel.green = green
-            pixel.blue = blue
+            get_grid_average(new_x, new_y, image) # combine with next line?
             image_copy.set_pixel(new_x, new_y, pixel_average(pixel, num))
             new_x += 1
         x += 2
@@ -38,18 +30,25 @@ def main():
     y += 2
 
     # image_copy.show()
+    """
 
 
 def get_grid_average(x, y, image):
     red = []
     green = []
     blue = []
+    counter = 0
     for i in range(x, x+2):
         for j in range(y, y+2):
             pixel = image.get_pixel(i, j)
             red.append(pixel.red)
             green.append(pixel.green)
             blue.append(pixel.blue)
+            counter += 1
+    pixel.red = sum(red) // counter
+    pixel.green = sum(green) // counter
+    pixel.blue = sum(blue) // counter
+    return pixel
 
 
 def pixel_average(pixel, num):
