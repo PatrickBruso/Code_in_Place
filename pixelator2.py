91060cceb1179eef4f99003ba90a4e1eef7206a7
Patrick <pkbruso@gmail.com>
2021-05-27 15:38:54 -0700

    Finished pixelator2.py to take image and replace each pixel with the closest color from a palette of colors.

--- pixelator2.py
+++ pixelator2.py
@@ -19,8 +19,8 @@ def main():
     """
     image = SimpleImage('images/Maddie.jpg')
     pixel_image = pixelate(image)  # call function that pixelates image
-    #image.show()
-    #pixel_image.show()
+    image.show()
+    pixel_image.show()
 
 
 def pixelate(image):
@@ -34,6 +34,10 @@ def pixelate(image):
         y = new_pixel.y
         old_pixel = image.get_pixel(x, y)
         palette_color = color_picker(old_pixel)
+        new_pixel.red = palette_color[0]
+        new_pixel.green = palette_color[1]
+        new_pixel.blue = palette_color[2]
+        image_copy.set_pixel(x, y, new_pixel)
     return image_copy
 
 
@@ -53,35 +57,49 @@ def color_picker(pixel):
                [255, 212, 163],
                [255, 236, 214]]
 
-    for i in range(len(palette)):
-        for value in palette[i]:
-            print(value[0])
-            print(value[1])
-            print(value[2])
-            distance_list = []
-            distance = math.sqrt((palette[i][j] - pixel.red) ** 2 + (color[1] - pixel.green) ** 2 + (color[2] - pixel.blue) ** 2)
-            # create pixels and put those into a list?
-            for x in range(8):
-                for y in range(8):
-                    pixel_list = [image.get_pixel(x, y)]
+    palette_2 = [[190, 74, 47],
+                 [215, 118, 67],
+                 [234, 212, 170],
+                 [228, 166, 114],
+                 [184, 111, 80],
+                 [115, 62, 57],
+                 [62, 39, 49],
+                 [162, 38, 51],
+                 [228, 59, 68],
+                 [247, 118, 34],
+                 [254, 174, 52],
+                 [254, 231, 97],
+                 [99, 199, 77],
+                 [62, 137, 72],
+                 [38, 92, 66],
+                 [25, 60, 62],
+                 [18, 78, 137],
+                 [0, 153, 219],
+                 [44, 232, 245],
+                 [255, 255, 255],
+                 [192, 203, 220],
+                 [139, 155, 180],
+                 [90, 105, 136],
+                 [58, 68, 102],
+                 [38, 43, 68],
+                 [24, 20, 37],
+                 [255, 0, 68],
+                 [104, 56, 108],
+                 [181, 80, 136],
+                 [246, 117, 122],
+                 [232, 183, 150],
+                 [194, 133, 105]]
 
-        # strip list items into separate integers?
-        x = 0  # counter maybe don't need
-        # compare palette[i] to pixel and then put distances into list.
-        # take list and find the smallest value.
-        # get the location in list of smallest value and match to palette list.
-        # return that value of palette list (for example palette[location of smallest in list]
-        #print(palette[i])
+    distance_list = []
 
-    """
-    for color in palette:
-        distance_list = []
-        distance = math.sqrt((color[0] - pixel.red) ** 2 + (color[1] - pixel.green) ** 2 + (color[2] - pixel.blue) ** 2)
+    for color in palette_2:
+        distance = int(math.sqrt((color[0] - pixel.red) ** 2 + (color[1] - pixel.green) ** 2 +
+                                 (color[2] - pixel.blue) ** 2))
         distance_list.append(distance)
-        print(distance_list)
-    # color_value = palette.get(pixel)
-    # return color_value
-    """
+
+    closest_color = min(distance_list)
+    location = distance_list.index(closest_color)
+    return palette_2[location]
 
 
 if __name__ == '__main__':
