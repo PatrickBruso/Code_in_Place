99fdd278c638b29c90032f3b737b518a55a62f3f
Patrick <pkbruso@gmail.com>
2021-08-26 16:04:29 -0700

    fixed issue with call to students.csv 'name'

--- OOP/oop2.py
+++ OOP/oop2.py
@@ -1,13 +1,30 @@
 """ Extra lesson in object-oriented programming """
 
+import csv
 from participant import Participant  # importing our Participant class from participant.py
+
+STUDENT_DATA = "students.csv"
+
+
+def read_student_data():
+
+    with open("students.csv") as f:
+        reader = csv.DictReader(f)
+
+        for line in reader:
+            student_name = line['ï»¿name']  # The CSV file has this as the name for some reason
+            student_email = line['email']
+            section_number = int(line['section_number'])
+
+            new_student = Participant(student_name, student_email, "student")
+
+            print(f"The participant's name is {new_student.name}")
+            print(f"The participant's email is {new_student.email}")
+            print(f"The participant's role is {new_student.role}")
 
 
 def main():
-    cip_participant = Participant("Brahm Capoor", "brahm@cs.stanford.edu", "section leader")
-    print(f"The participant's name is {cip_participant.name}")
-    print(f"The participant's email is {cip_participant.email}")
-    print(f"The participant's role is {cip_participant.role}")
+    read_student_data()
 
 
 if __name__ == "__main__":
